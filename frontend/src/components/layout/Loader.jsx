f4f2ef0c4d8917c3e3c71e62672df755094106af
Abutalib Khan <thetalibkhan555@gmail.com>
2023-01-04 19:25:04 +0530

    backend

--- frontend/src/components/layout/Loader.jsx
+++ frontend/src/components/layout/Loader.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {IoFastFoodOutline} from "react-icons/io5";
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className='loader'>
+
+        <IoFastFoodOutline />
+
+    <div>
+        <p>
+            Loading...
+        </p>
+    </div>
+
+
+
+
+    </div>
   )
 }
 
